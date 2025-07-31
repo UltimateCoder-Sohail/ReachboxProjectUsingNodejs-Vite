ee1452eee8bd9eac58f8a3951b2d2d987733fdb8
UltimateCoder-Sohail <sohailakhtar11sci@gmail.com>
2025-07-31 14:54:09 +0530

    .

--- src/EmailList.tsx
+++ src/EmailList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import DataTable from 'react-data-table-component';
 import { fetchEmails ,classifyEmail ,generateReply } from './api';
 
