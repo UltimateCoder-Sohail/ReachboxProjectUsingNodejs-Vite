a55b1e4cf05f2f389cceed593c35569d62cebe59
UltimateCoder-Sohail <sohailakhtar11sci@gmail.com>
2025-07-31 14:55:28 +0530

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import EmailList from './Emaillist'; // ❌ Wrong casing
+import EmailList from './EmailList'; // ❌ Wrong casing
 
 import './App.css'
 
